eee68a3372483fda6aa103fb21fbd316ffbba73a
mgatto <none@none>
2009-01-15 19:10:16 -0700

    * Code sniffing results and changes since previous code sniffing.

--- Trunk/VersionControl/Hg/Repository.php
+++ Trunk/VersionControl/Hg/Repository.php
@@ -1,25 +1,34 @@
 <?php
     /**
      * Contains the Repository class.
+     *
+     * @category VersionControl
+     * @package Hg
+     * @subpackage Repository
+     * @author Michael Gatto <mgatto@u.arizona.edu>
+     * @license PHP
      */
-
 
     /**
      * Represents the Repository as an entity.
      *
      * Usage:
      *
-     * $m->repo()->load('/path/');
-     * $m->repo()->create('/path/');
+     * $repo = new Repository();
+     * $repo->load( '/path/' )
      *
-     * $m = hg::init()->load('/path/');
-     * $m = hg::init()->create('/path/');
+     * As a child of Hg.php:
+     * $hg = new Hg();
+     * $repo = $hg->getRepository( '/path/' );
      *
+     * Or:
+     * $repo = hg::instance()->load('/path/');
+     * $repo = hg::instance()->create('/path/');
      *
+     * @category VersionControl
+     * @package Hg
+     * @subpackage Repository
      * @author Michael Gatto <mgatto@u.arizona.edu>
-     * @package
-     * @subpackage
-     * @category
      * @license PHP
      */
     class Repository
@@ -31,117 +40,52 @@
          *
          * @const string
          */
-        const REPO_ROOT_DIRECTORY_NAME = '/.hg';
+        const HG_ROOT_DIRECTORY_NAME = '/.hg';
 
         /**
-         * Designates the repository as on the local filesystem.
-         *
-         * @const integer
-         */
-        const HG_LOCAL_REPOSITORY = 1;
-
-        /**
-         * Designates the repository as on a remote system.
-         *
-         * @const integer
-         */
-        const HG_REMOTE_REPOSITORY = 1;
-
-        /**
-         * alias for $_path?
+         * Holds the filesystem path of a Mercurial repository.
          *
          * @var string
          */
-        private $_uri;
-        private $_remote_transport;
-        private $_remote_username;
-        private $_remote_password;
+        private $_path;
 
         /**
-         * The file to be operated upon with relation to the repository.
+         * Holds the current command operating on the repository.
          *
          * @var string
          */
-        private $_file;
-
-        public function __construct( $path )
-        {
-            /*
-             * Remember that the owner of the directory has to be the same of the script user,
-             * otherwise this function will always return false when PHP is running in safe_mode..
-             */
-            $this->setPath( $path );
-
-            if ( ! $this->isRepository( $this->getPath() ) ) {
-                throw new Exception( 'there is no Mercurial repository at: ' . $this->getPath() );
-            }
-        }
+        private $_command;
 
         /**
-         * Create a new Mercurial repository.
+         * Repository constructor which currently does nothing.
          *
-         * Implements `hg init`
-         *
-         * @param $uri the path for the repository to be created
-         * @return boolean true on success, false on failure
+         * @todo might be a good place to set the transport method?
          */
-        public function create( $uri )
-        {
-            //call isRepository and throw an exception if a repo already exists here, or let `hg init` do it for me?
-
-            //create a command object and run it. it should return an exception type if it borked.
-
-            exec( 'hg init' . $options, $output );
-
-
-            $this->setPath( $uri );
-        }
+        public function __construct() { }
 
         /**
-         * Set an existing Mercurial repository
+         * Sets the path of a Mercurial repository after validating it as a Hg repo.
          *
-         * @param $uri the path for the repository to be used in subsequent ops.
-         * @return boolean true on success, false on failure
-         */
-        public function load( $uri ) {} //accepts file uris for now; load_from_url(); load_from_filesystem()
-
-        public function delete() {}
-        public function move_to() {}
-        public function rename_to() {}
-        public function clone_from()
-        {
-            //call load()
-            //call create()
-        }
-
-        private function construct_url()
-        {
-            //require PEAR/Net/URL2.php
-        }
-
-        /**
-         *
-         * @param string $path
+         * @param string $path as a local filesystem path.
+         * @return Repository to enable method chaining
          * @see $_path
-         * @assert ( $path ) === true
          */
         public function setPath( $path )
         {
-            //@todo add checking the directory
+            $repo = $path . self::HG_ROOT_DIRECTORY_NAME;
 
-            //I don't want empty paths
-            if ( ! empty( $path ) && is_dir( $path ) ) {
-                $this->_path = $path;
-                return true;
-            }
-            else {
-                throw new Exception( 'A path is required' );
+            if ( ! $this->isRepository( $repo ) ) {
+                throw new Exception( 'This path is not a valid Mercurial repository' );
             }
 
-            //@todo return $this so we can chain the methods.
+            $this->_path = $repo;
+            return this; //for chainable methods.
+
         }
 
         /**
+         * Returns the path of a Mercurial repository as set by the user.
+         * It is validated before being set as a class member.
          *
          * @return string
          * @see $_path
@@ -156,9 +100,17 @@
             }
         }
 
+        /**
+         * Checks if $this is in fact a valid
+         *
+         * @return boolean
+         */
         public function isRepository()
         {
-            if ( is_dir( $this->getPath() . $this->_repoRootDirectoryName ) ) {
+            /*
+             * both conditions must be satisfied.
+             */
+            if ( ! ( is_dir( $repo ) && empty( $repo ) ) ) {
                 return true;
             }
             else {
@@ -167,20 +119,86 @@
         }
 
         /**
-         * Set the specific file within the repository (or, working copy??) to operated upon
+         * Set an existing Mercurial repository to use
          *
-         * @return Repository
-         * @see $_file
+         * Usage:
+         *  $repo = new Repository();
+         *  $repo->load( '/path/' );
+         *  Or:
+         *  $repo->setPath( '/path/' )->load();
+         *
+         * @todo change to accept a path on its own or getPath when chained
+         *
+         * @param $uri the path for the repository to be used in subsequent ops.
+         * @return boolean true on success, false on failure
          */
-        public function getFile( $file )
+        public function load( $path )
         {
-            //should raise exception if the file is not versioncontrolled?
-            //@todo How to find out? Run Status.php and ensure $file is in_array()?
+            /*
+             * Remember that the owner of the directory has to be the same of the script user,
+             * otherwise this function will always return false when PHP is running in safe_mode..
+             */
+            $this->setPath( $path );
 
-            $this->_file = $file; //will be overwritten each time function is called.
+            /*
+             * Let's not guess that the user wants to create a repo if none exists;
+             * Throw and exception and let them decide what to do next.
+             * Maybe they just gave the wrong path.
+             */
+            if ( ! $this->isRepository( $this->getPath() ) ) {
+
+                throw new Exception( 'there is no Mercurial repository at: '
+                    . $this->getPath()
+                    . ' Use $hg->createRepository( \'/path/\' ) to create one and then use getRepository() to act upon it.' );
+            }
+
             return $this;
         }
 
+        /**
+         * Create a new Mercurial repository.
+         *
+         * Implements `hg init`
+         *
+         * @param $uri the path for the repository to be created
+         * @return boolean true on success, false on failure
+         */
+        public function create( $path )
+        {
+            //call isRepository and throw an exception if a repo already exists here, or let `hg init` do it for me?
+
+            //create a command object and run it. it should return an exception type if it borked.
+
+            if ( ! $this->getPath() ) {
+
+            }
+
+            $this->_command = new Init( $path );
+
+        }
+
+        /**
+         * Deletes the repository, effectively removing the working copy from SCM.
+         *
+         * @return boolean
+         */
+        public function delete()
+        {
+            if ( unlink(  ) ) {
+                return true;
+            } else {
+                return false;
+                //throw new Exception( 'The repository could not be deleted.' );
+            }
+
+        }
+
+        /**
+         *
+         * @param $function
+         * @param $options
+         * @return unknown_type
+         */
         public function __call( $function, $options )
         {
             if( class_exists( $function ) ) {
