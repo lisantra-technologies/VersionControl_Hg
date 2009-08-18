9de41616aecda32e83ed7ccf062e4d32f002e7ee
mgatto <none@none>
2009-08-18 16:04:14 -0700

    * Switched location of trace results to within Documentation, since that's kind of what they are...
    --HG--
    rename : Trunk/Scripts/Benchmarks/test_HgConstructor_InvalidRepository.php_trace.xt => Trunk/Documentation/Benchmarks/test_HgConstructor_InvalidRepository.php_trace.xt
    rename : Trunk/Scripts/Benchmarks/test_HgConstructor_NonexistantPath.php_trace.xt => Trunk/Documentation/Benchmarks/test_HgConstructor_NonexistantPath.php_trace.xt
    rename : Trunk/Scripts/Benchmarks/test_HgConstructor_NullParams.php_trace.xt => Trunk/Documentation/Benchmarks/test_HgConstructor_NullParams.php_trace.xt
    rename : Trunk/Scripts/Benchmarks/test_HgConstructor_ValidRepository.php_trace.xt => Trunk/Documentation/Benchmarks/test_HgConstructor_ValidRepository.php_trace.xt

--- Trunk/Scripts/Benchmarks/benchmark_hg_constructor.php
+++ Trunk/Scripts/Benchmarks/benchmark_hg_constructor.php
@@ -7,7 +7,7 @@ $base_path = "../../Tests/Functional";
 $includes = glob($base_path . DIRECTORY_SEPARATOR . "test_HgConstructor_*.php");
 
 foreach ($includes as $test) {
-    xdebug_start_trace('./' . basename($test) . '_trace'); //2nd param = 1 for append mode
+    xdebug_start_trace('../../Documentation/Benchmarks/' . basename($test) . '_trace'); //2nd param = 1 for append mode
         include $test;
     xdebug_stop_trace();
 }
