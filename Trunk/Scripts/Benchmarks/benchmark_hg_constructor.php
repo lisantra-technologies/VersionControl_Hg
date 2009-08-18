d6cff7e77ae95d89046206e6db2a5d3a021deec3
mgatto <none@none>
2009-08-18 13:12:41 -0700

    * Added basic benchmark tests using Xdebug's tracing by including the functional test files.

---
+++ Trunk/Scripts/Benchmarks/benchmark_hg_constructor.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once '../../VersionControl/Hg.php';
+
+$base_path = "../../Tests/Functional";
+
+$includes = glob($base_path . DIRECTORY_SEPARATOR . "test_HgConstructor_*.php");
+
+foreach ($includes as $test) {
+    xdebug_start_trace('./' . basename($test) . '_trace'); //2nd param = 1 for append mode
+        include $test;
+    xdebug_stop_trace();
+}
