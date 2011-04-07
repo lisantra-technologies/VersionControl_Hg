65912639c044a113d73525bea364e5c79eda6f72
Michael Gatto <mgatto2@lisantra.com>
2011-04-07 15:00:29 -0700

    * Added error constants and messages for more structured and consistent handline and error output. * Remove HG Keyword: its useless and just forces a state change in the file after committing.

--- Trunk/VersionControl/Hg/Command/Exception.php
+++ Trunk/VersionControl/Hg/Command/Exception.php
@@ -11,7 +11,6 @@
  * @author      Michael Gatto <mgatto@lisantra.com>
  * @copyright   2009 Lisantra Technologies, LLC
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version     Hg: $Revision$
  * @link        http://pear.php.net/package/VersionControl_Hg
  */
 
@@ -26,11 +25,31 @@
  * @author      Michael Gatto <mgatto@lisantra.com>
  * @copyright   2009 Lisantra Technologies, LLC
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version     Hg: $Revision$
  * @link        http://pear.php.net/package/VersionControl_Hg
  */
-class VersionControl_Hg_Command_Exception
-    extends VersionControl_Hg_Exception
+class VersionControl_Hg_Command_Exception extends Exception
 {
+    /**
+     * Error constant
+     */
+    const COMMANDLINE_ERROR = 'commandLineError';
 
+    /**
+     * Error messages for humans
+     *
+     * @var array
+     */
+    protected $_messages = array(
+        'commandLineError' => "The command line returned an error status of '0'. Please examine the output of \$object->getCommandString() to see the actual shell command issued.",
+    );
+
+    /**
+     * Override constructor so we can make exception messages more structured
+     * like Zend Framework's.
+     *
+     * @param string $message is equivalent to the error constants
+     */
+    public function __construct($message) {
+        parent::__construct($this->_messages[$message]);
+    }
 }
