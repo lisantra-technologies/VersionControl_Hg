dc9f8abae391c068704c88e301c8c927423e7977
Michael Gatto <mgatto2@lisantra.com>
2011-05-18 16:43:18 -0700

    * Class name was missing 'Container'. Its probably a signal that it ought to be removed and all subdirs and files moved one level up.

--- Trunk/VersionControl/Hg/Container/Repository/Revision.php
+++ Trunk/VersionControl/Hg/Container/Repository/Revision.php
@@ -43,7 +43,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @link       http://pear.php.net/package/VersionControl_Hg
  */
-class VersionControl_Hg_Repository_Revision
+class VersionControl_Hg_Container_Repository_Revision
 {
     /**
      * Revision identifier is an integer
